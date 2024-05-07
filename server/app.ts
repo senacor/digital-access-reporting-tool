e8648c80659bf2399bf604c48ab5feae444432c7
edhelb <edrian.helbing@gmail.com>
2024-05-07 09:17:32 +0200

    Add website url crawler

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import * as aChecker from "accessibility-checker";
+import * as cheerio from "cheerio";
+import fetch from "node-fetch";
 
 const app = express();
 const port = process.env.PORT || 42069;
@@ -9,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+app.get("/", (_, res) => {
   res.send({ hello: "world" });
 });
 
@@ -21,8 +23,58 @@ app.post("/achecker", (req, res) => {
   }
 
   aChecker.getCompliance(url, url, function (results) {
+    aChecker.close();
     res.send({ results });
   });
 });
+
+app.post("/crawler", async (req, res) => {
+  const url: string = req.body.url;
+
+  if (!url) {
+    return res.status(400).send({ error: "URL is required" });
+  }
+
+  const URLs: Set<string> = new Set();
+  await crawlUrls(url, URLs);
+
+  res.send({ urls: Array.from(URLs) });
+});
+
+async function crawlUrls(url: string, crawledUrls: Set<string>) {
+  try {
+    const response = await fetch(url);
+
+    if (response.ok) {
+      if (!crawledUrls.has(url)) {
+        crawledUrls.add(url);
+      }
+
+      const data = await response.text();
+      const $ = cheerio.load(data);
+
+      $("a").each((_, link) => {
+        const href = $(link).attr("href");
+
+        if (!href) {
+          return;
+        }
+
+        // Resolve relative URLs
+        const fullUrl = new URL(href, url);
+        const isSameDomain = fullUrl.hostname === new URL(url).hostname;
+        const hasNotBeenCrawled = !crawledUrls.has(fullUrl.href);
+
+        if (isSameDomain && hasNotBeenCrawled) {
+          crawledUrls.add(fullUrl.href);
+        }
+      });
+    } else {
+      console.error("Error:", response.status);
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
--- src/App.tsx
+++ src/App.tsx
@@ -15,10 +15,13 @@ const renderTestChart = () => {
 };
 
 function App() {
-  const [url, setUrl] = useState("http://www.google.com");
-  const [results, setResults] = useState<unknown | null>(null);
+  const [url, setUrl] = useState("http://www.devk.de/");
+  const [acsResults, setAcsResults] = useState<unknown | null>(null);
+  const [acsResultsHidden, setAcsResultsHidden] = useState(false);
+  const [websiteUrls, setWebsiteUrls] = useState<string[]>([]);
+  const [websiteUrlsHidden, setWebsiteUrlsHidden] = useState(false);
 
-  const handleSubmit = async () => {
+  const handleCheckAccessibility = async () => {
     const response = await fetch("/api/achecker", {
       method: "POST",
       headers: {
@@ -27,26 +30,71 @@ function App() {
       body: JSON.stringify({ url }),
     });
     const data = await response.json();
+    setAcsResults(data);
     console.log(data);
-    setResults(data);
+  };
+
+  const handleCrawlWebsiteUrls = async () => {
+    const response = await fetch("/api/crawler", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ url }),
+    });
+    const data = await response.json();
+    setWebsiteUrls(data);
+    console.log(data);
   };
 
   return (
-    <div>
-      <input
-        type="text"
-        name="url"
-        value={url}
-        onChange={(e) => setUrl(e.target.value)}
-        onKeyDown={(e) => {
-          if (e.key === "Enter") {
-            handleSubmit();
-          }
-        }}
-      />
-      <button onClick={handleSubmit}>Submit</button>
-      <pre>{JSON.stringify(results)}</pre>
-      {renderTestChart()}
+    <div style={{ display: "flex", flexDirection: "column", gap: "8px" }}>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <input
+          type="text"
+          name="url"
+          placeholder="http://www.example.com"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+          onKeyDown={(e) => e.key === "Enter" && handleCheckAccessibility()}
+        />
+
+        <div style={{ display: "flex", gap: "4px", justifyContent: "center" }}>
+          <button onClick={handleCheckAccessibility}>
+            Check accessibility
+          </button>
+          <button onClick={handleCrawlWebsiteUrls}>Crawl website urls</button>
+        </div>
+
+        <div style={{ display: "flex", flexDirection: "column", gap: "4px" }}>
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <h2>Accessibility Results</h2>
+            <button onClick={() => setAcsResultsHidden(!acsResultsHidden)}>
+              {acsResultsHidden ? "Show" : "Hide"}
+            </button>
+          </div>
+          {!acsResultsHidden && (
+            <pre style={{ textAlign: "left" }}>
+              {JSON.stringify(acsResults, null, 2)}
+            </pre>
+          )}
+        </div>
+
+        <div>
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <h2>Website Urls</h2>
+            <button onClick={() => setWebsiteUrlsHidden(!websiteUrlsHidden)}>
+              {websiteUrlsHidden ? "Show" : "Hide"}
+            </button>
+          </div>
+          {!websiteUrlsHidden && (
+            <pre style={{ textAlign: "left" }}>
+              {JSON.stringify(websiteUrls, null, 2)}
+            </pre>
+          )}
+        </div>
+      </div>
+      <div>{renderTestChart()}</div>
     </div>
   );
 }
