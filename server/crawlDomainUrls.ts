74c7943e5b1d0d56e06a4a6a32f3c415756a3512
edhelb <edrian.helbing@gmail.com>
2024-05-29 14:28:54 +0200

    Refine crawler function and make it recursive

--- server/crawlDomainUrls.ts
+++ server/crawlDomainUrls.ts
@@ -2,41 +2,97 @@ import * as cheerio from "cheerio";
 import fetch from "node-fetch";
 
 export async function crawlDomainUrls(url: string) {
-  const crawledUrls: Set<string> = new Set();
+  const urls = new Set<string>();
+
+  await crawlDomainUrlsRecursively(url, urls);
+
+  return Array.from(urls);
+}
+
+async function crawlDomainUrlsRecursively(
+  url: string,
+  crawledUrls: Set<string>,
+) {
+  if (crawledUrls.has(url)) {
+    return;
+  }
+
+  crawledUrls.add(url);
+
+  const uniqueUrlsFoundOnSite = await findUniqueSameDomainUrlsOnSite(url);
+  const urlArray = Array.from(uniqueUrlsFoundOnSite);
+
+  for (let i = 0; i < urlArray.length; i += 5) {
+    const urlsToCrawl = urlArray.slice(i, i + 5);
+
+    await Promise.all(
+      urlsToCrawl.map(async (url) => {
+        await crawlDomainUrlsRecursively(url, crawledUrls);
+      }),
+    );
+  }
+}
+
+const excludedUrlPrefixes = ["mailto:", "tel:", "#"];
+
+const fileExtensions = [
+  ".pdf",
+  ".jpg",
+  ".jpeg",
+  ".png",
+  ".gif",
+  ".svg",
+  ".zip",
+];
+
+const findUniqueSameDomainUrlsOnSite = async (siteUrl: string) => {
+  const urlsFoundOnSite = new Set<string>();
 
   try {
-    const response = await fetch(url);
+    const siteResponse = await fetch(siteUrl);
 
-    if (response.ok) {
-      if (!crawledUrls.has(url)) {
-        crawledUrls.add(url);
-      }
-
-      const data = await response.text();
-      const $ = cheerio.load(data);
+    if (siteResponse.ok) {
+      const siteHtml = await siteResponse.text();
+      const $ = cheerio.load(siteHtml);
 
       $("a").each((_, link) => {
-        const href = $(link).attr("href");
+        let href = $(link).attr("href");
 
         if (!href) {
           return;
         }
 
-        // Resolve relative URLs
-        const fullUrl = new URL(href, url);
-        const isSameDomain = fullUrl.hostname === new URL(url).hostname;
-        const hasNotBeenCrawled = !crawledUrls.has(fullUrl.href);
+        if (excludedUrlPrefixes.some((prefix) => href?.startsWith(prefix))) {
+          return;
+        }
 
-        if (isSameDomain && hasNotBeenCrawled) {
-          crawledUrls.add(fullUrl.href);
+        if (href.includes("#")) {
+          href = href.split("#")[0]; // Remove anchor links so we don't crawl the same page multiple times
+        }
+
+        if (href.includes("?")) {
+          href = href.split("?")[0]; // Remove query params so we don't crawl the same page multiple times
+        }
+
+        if (href.includes(";")) {
+          href = href.split(";")[0]; // Remove query params so we don't crawl the same page multiple times
+        }
+
+        const fullUrl = new URL(href, siteUrl); // Resolve relative URLs
+        const isSameDomain = fullUrl.hostname === new URL(siteUrl).hostname;
+        const hasNotBeenFoundYet = !urlsFoundOnSite.has(fullUrl.href);
+        const isFileUrl = fileExtensions.some((ext) =>
+          fullUrl.pathname.endsWith(ext),
+        );
+
+        if (isSameDomain && hasNotBeenFoundYet && !isFileUrl) {
+          urlsFoundOnSite.add(fullUrl.href);
         }
       });
-    } else {
-      console.error("Error:", response.status);
     }
   } catch (error) {
-    console.error("Error:", error);
+    console.error(error);
   }
 
-  return Array.from(crawledUrls);
-}
+  return urlsFoundOnSite;
+};
