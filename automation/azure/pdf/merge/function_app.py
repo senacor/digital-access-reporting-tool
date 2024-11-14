6e6a853efa0a63445ab06f38e75c4e91153a7f14
Petr Andreev <petr.andreev@senacor.com>
2024-11-14 18:07:20 +0100

    feat(add-automation): re-deployed Azure functions, fixed refresh

--- automation/azure/pdf/merge/function_app.py
+++ automation/azure/pdf/merge/function_app.py
@@ -7,6 +7,7 @@ import pypdfium2 as pdfium
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
+@app.function_name(name="MergePDFs")
 @app.route(route="merge")
 def merge(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
